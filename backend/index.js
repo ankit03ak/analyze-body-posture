a44efdcfb9826efa564b78b1f27843a59bccb23a
Ankit Kumar <vickyak1339@gmail.com>
2025-07-11 10:12:30 +0530

    added frontend deployed url to backend

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,17 @@ const videoAnalyzeRoute = require("./routes/videoAnalyzeRoute.js");
 const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
-  //frontend will be added
-
+  "https://analyze-body-posture.vercel.app"
 ];
 
 app.use(cors({
   origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
+    if (!origin || allowedOrigins.includes(origin)) return callback(null, true);
     return callback(new Error("Not allowed by CORS"));
   },
-  methods: ["GET", "POST"],
   credentials: true
 }));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
